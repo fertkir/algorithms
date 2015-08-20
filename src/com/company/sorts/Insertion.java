a3a16e08d32d9b5c064cb1b87e5f30640089c960
Kirill Fertikov <kirill.fertikov@gmail.com>
2015-08-20 23:12:30 +0300

    Insertion sort tidy up

--- src/com/company/sorts/Insertion.java
+++ src/com/company/sorts/Insertion.java
@@ -8,14 +8,12 @@ public class Insertion {
     public static void sort(int[] arr) {
         for (int i = 1; i < arr.length; i++) {
             int current = arr[i];
-            int j;
-            for (j = i - 1; j >= 0; j--) {
-                if (arr[j] < current) {
-                    break;
-                }
-                arr[j + 1] = arr[j];
+            int holeId = i;
+            while (holeId > 0 && arr[holeId-1] > current) {
+                arr[holeId] = arr[holeId-1];
+                holeId--;
             }
-            arr[j + 1] = current;
+            arr[holeId] = current;
         }
     }
 }
