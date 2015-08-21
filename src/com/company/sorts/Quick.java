8baf4159999ecb73b1c96ba8a1fa3e09becc03f3
Kirill Fertikov <kirill.fertikov@gmail.com>
2015-08-21 13:36:24 +0300

    Quick sort implemented

--- src/com/company/sorts/Quick.java
+++ src/com/company/sorts/Quick.java
@@ -10,14 +10,23 @@ public class Quick {
     }
 
     private static void sort(int[] arr, int start, int end) {
-        int pivot = end;
-//        for () {
-//
-//        }
+        if (start < end) {
+            int pivot = partition(arr, start, end);
+            sort(arr, start, pivot - 1);
+            sort(arr, pivot + 1, end);
+        }
     }
 
-    private static void partition() {
-
+    private static int partition(int[] arr, int start, int end) {
+        int pIndex = start;
+        for (int i = start; i < end; i++) {
+            if (arr[i] <= arr[end]) {
+                swap(arr, i, pIndex);
+                pIndex++;
+            }
+        }
+        swap(arr, pIndex, end);
+        return pIndex;
     }
 
     private static void swap(int[] arr, int ind1, int ind2) {
