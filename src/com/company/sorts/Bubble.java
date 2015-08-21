65b5272949860af7267b606a65aac61d910889b5
Kirill Fertikov <kirill.fertikov@gmail.com>
2015-08-21 20:45:18 +0300

    Bubble minor

--- src/com/company/sorts/Bubble.java
+++ src/com/company/sorts/Bubble.java
@@ -6,9 +6,9 @@ package com.company.sorts;
 public class Bubble {
 
     public static void sort(int[] arr) {
-        for (int k = 0; k < arr.length - 1; k++) {
+        for (int k = 1; k < arr.length; k++) {
             boolean sorted = true;
-            for (int i = 0; i < arr.length - k - 1; i++) {
+            for (int i = 0; i < arr.length - k; i++) {
                 if (arr[i] > arr[i+1]) {
                     int tmp = arr[i];
                     arr[i] = arr[i+1];
