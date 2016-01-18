e5a1db957b81e5f7d170fba1950839d79658eff6
Kirill Fertikov <kirill.fertikov@gmail.com>
2016-01-19 00:57:59 +0300

    added some bitwise problems

--- src/com/company/bitwise/Bitwise.java
+++ src/com/company/bitwise/Bitwise.java
@@ -1,22 +1,24 @@
 package com.company.bitwise;
 
+import static java.lang.System.out;
+
 /**
  * Created by fertkir on 18.01.16.
  */
 public class Bitwise {
 
     public static void main(String[] args) {
-        System.out.println(numberOfOnes(9));
-        System.out.println(findOddWhenOthersEven(new int[] {2, 3, 2, 2, 3, 6, 5, 5, 5, 6, 2}));
-        System.out.println(findUniqueWhenTriple(new int[] {2, 3, 6, 2, 3, 6, 3, 5, 6, 2}));
+        out.println(numberOfOnes(9));
+        out.println(findOddWhenOthersEven(new int[] {2, 3, 2, 2, 3, 6, 5, 5, 5, 6, 2}));
+        out.println(findUniqueWhenTriple(new int[] {2, 3, 6, 2, 3, 6, 3, 5, 6, 2}));
+        out.println(bitsToConvert1(31, 14));
+        out.println(bitsToConvert2(31, 14));
+        out.println(Integer.toBinaryString(swapEvenOddBits(0b011010110101)));
     }
 
     /**
      * Please implement a function to get the number of 1s in an integer.
      * For example, the integer 9 is 1001 in binary, so it returns 2 since there are two bits of 1s.
-     *
-     * @param value
-     * @return
      */
     public static int numberOfOnes(int value) {
         int count = 0;
@@ -30,9 +32,6 @@ public class Bitwise {
     /**
      * In an array, all numbers appear even number of times except one which appears odd number of times.
      * Please find the unique number.
-     *
-     * @param arr
-     * @return
      */
     public static int findOddWhenOthersEven(int[] arr) {
         int unique = 0;
@@ -45,9 +44,6 @@ public class Bitwise {
     /**
      * In an array, all numbers appear three times except one which only appears only once.
      * Please find the unique number.
-     *
-     * @param arr
-     * @return
      */
     static int findUniqueWhenTriple(int[] arr) {
         int result = 0;
@@ -61,5 +57,28 @@ public class Bitwise {
             }
         }
         return result;
+    }
+
+    /**
+     * Write a function to determine the number of bits required to convert integer A to integer B.
+     */
+    public static int bitsToConvert1(int a, int b) {
+        return numberOfOnes(a^b);
+    }
+
+    public static int bitsToConvert2(int a, int b) {
+        int count = 0;
+        for (int xor = a ^ b; xor != 0; xor >>= 1) {
+            count += xor & 1;
+        }
+        return count;
+    }
+
+    /**
+     * Write a program to swap odd and even bits in an integer with as few instructions as
+     * possible (e.g., bit 0 and bit 1 are swapped, bit 2 and bit 3 are swapped, etc).
+     */
+    public static int swapEvenOddBits(int value) {
+        return ((value & 0x55555555) << 1) | ((value & 0xAAAAAAA) >> 1);
     }
 }
